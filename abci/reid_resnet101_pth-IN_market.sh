d9e6da23b27ac78dbcfdcdcc2abda207261e1b13
Yoshihiro Fukuhara <gatheluck+tech@gmail.com>
2019-03-07 23:30:54 +0900

    add scripts for abci

---
+++ abci/reid_resnet101_pth-IN_market.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#$ -l rt_F=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -N reid_res101_pth-IN_market
+#$ -o /home/aaa10329ah/user/waseda/abci_log/reid_resnet101_pth-IN_market.o
+
+source /etc/profile.d/modules.sh
+module load cuda/9.0/9.0.176.4
+export PATH="/home/aaa10329ah/anaconda3/bin:${PATH}"
+source activate waseda
+cd /home/aaa10329ah/user/waseda/person-reid
+# script
+
+python run_all.py --gpu_ids 0,1,2,3 \
+									--data_dir data/Market/pytorch/ \
+									--train_all \
+									--num_epochs 50	\
+									--batchsize 16 \
+									-a resnet101 \
+									--bb_weight data/models/resnet101_pth-IN.pth \
+									-l logs/resnet101_pth-IN_market
---
+++ abci/reid_resnet50_rge-SIN_market.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#$ -l rt_F=1
+#$ -l h_rt=72:00:00
+#$ -j y
+#$ -N reid_res50_rge-SIN_market
+#$ -o /home/aaa10329ah/user/waseda/abci_log/reid_resnet50_rge-SIN_market.o
+
+source /etc/profile.d/modules.sh
+module load cuda/9.0/9.0.176.4
+export PATH="/home/aaa10329ah/anaconda3/bin:${PATH}"
+source activate waseda
+cd /home/aaa10329ah/user/waseda/person-reid
+# script
+
+python run_all.py --gpu_ids 0,1,2,3 \
+									--data_dir data/Market/pytorch/ \
+									--train_all \
+									--num_epochs 50	\
+									--batchsize 32 \
+									-a resnet50 \
+									--bb_weight data/models/resnet50_rge-SIN.pth \
+									-l logs/resnet50_rge-SIN_market
